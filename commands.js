2d0e6ad3de630c50037d53bf81e672f58b5089ad
BeastBreath <nividh.singh@gmail.com>
2025-03-20 10:34:47 -0700

    Updated formatting

---
+++ commands.js
@@ -0,0 +1,19 @@
+// commands.js
+const helpCommands = [
+  {
+    command: 'help',
+    description: "List all commands (you're looking at it)"
+  },
+  {
+    command: 'aboutme',
+    description: "Learn about me!"
+  },
+  {
+    command: 'clear',
+    description: "Clear the terminal screen"
+  },
+  {
+    command: 'exit',
+    description: "Switch to the web version"
+  }
+];

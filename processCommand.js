15fd8ae12e247e7f29d02c8b75394be9b8ecff62
Nividh S <nividh.singh@gmail.com>
2025-03-26 18:47:31 -0400

    A lot of changes but now it looks pretty good and is almost done

--- processCommand.js
+++ processCommand.js
@@ -1,21 +1,20 @@
-// processCommand.js
 function processCommand(cmd) {
   switch (cmd.toLowerCase()) {
     case "help":
       let tableHTML = `<table class="help-table">`;
       helpCommands.forEach((item) => {
         tableHTML += `
-              <tr>
-                <td class="help-command">${item.command}</td>
-                <td class="help-description">${item.description}</td>
-              </tr>`;
+            <tr>
+              <td class="help-command">${item.command}</td>
+              <td class="help-description">${item.description}</td>
+            </tr>`;
       });
       tableHTML += `</table>`;
       return tableHTML;
 
     case "about me":
-      return `Hi! My name I'm a sophomore at Olin College of Engineering near Boston. 
-  I’ve been building software for 6 years. I love building cool stuff and challenging myself, whether that be making a self-balancing cube or completing an Ironman. In my free time I enjoy watching and playing basketball and F1, riding horses, and gokarting.`;
+      return `Hi! I'm a sophomore at Olin College of Engineering near Boston. 
+I’ve been building software for 6 years. I love building cool stuff and challenging myself, whether that be making a self-balancing cube or completing an Ironman. In my free time I enjoy watching and playing basketball and F1, riding horses, and gokarting.`;
 
     case "clear":
       if (typeof window.clearTerminal === "function") {
@@ -42,23 +41,24 @@ function processCommand(cmd) {
       return `<a href="${linkedinLink}" target="_blank" rel="noopener noreferrer">View Linkedin</a>`;
 
     case "tldr":
-      // Return a table with all TLDR items
       return generateTldrTable(tldrItems);
 
     case "tldr projects":
-      // Filter and return only projects
       return generateTldrTable(
         tldrItems.filter((item) => item.type === "project")
       );
 
     case "tldr internships":
-      // Filter and return only internships
       return generateTldrTable(
         tldrItems.filter((item) => item.type === "internship")
       );
 
+    case "exit":
+      // Redirect to the web version
+      window.location.href = "web.html";
+      return "";
+
     default:
-      // Optionally, if the command starts with "tldr " handle specific TLDR requests later
       if (cmd.toLowerCase().startsWith("tldr ")) {
         return "Specific TLDR command not implemented yet.";
       }
